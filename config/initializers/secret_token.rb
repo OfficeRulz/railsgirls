45a1d9440ed1f36c5608b8c629d7bbdb3b72ab20
Christine Smith <christine@officerulz.com.au>
2013-05-25 16:04:42 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = 'b236e64e2eb7a815ddb7992abf4f30e17d9d07ea66b311781fb996a6957c1f63bf19b565dd54bc1785ec4a3511d411d66d52dab1a7189b4ab80599c5a2373e80'
